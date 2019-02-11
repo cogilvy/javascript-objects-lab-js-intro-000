a4d88f1b4744b60f2dc47d91de1e4f4644f74627
Chris Ogilvy <chrisogilvy@cox.net>
2019-02-11 03:52:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {key: "value"};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key]: value})
+  return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
